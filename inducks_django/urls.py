803fb161cc7b1b25004f68cc905e801060144adc
Anders Birkenes <datagutten@datagutten.net>
2026-08-10 14:08:26 +0200

    Add static media hosting for debug server

--- inducks_django/urls.py
+++ inducks_django/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -27,3 +28,7 @@ urlpatterns = [
 
 if settings.DEBUG_TOOLBAR:
     urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
